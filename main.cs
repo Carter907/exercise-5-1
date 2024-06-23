e8dbf59ea25d776e50ca770bed9f9d084b28dd75
Carter <102479896+Carter907@users.noreply.github.com>
2024-06-23 19:16:43 +0000

    Added Arrays for initials, sales, and salesPeople

--- main.cs
+++ main.cs
@@ -1,39 +1,53 @@
 using System;
 using System.Linq;
 // Now, modify the program to use arrays to store the salesperson names, allowed initials, and accumulated sales values.
-class Program {
-  public static void Main(string[] args) {
-    var allowedInitials = new char[] {'d', 'e', 'f'};
-    
+class Program
+{
+  public static void Main(string[] args)
+  {
+    var allowedInitials = new char[] { 'd', 'e', 'f' };
+    var salesPeople = new char[3];
+    var accumulatedSales = new int[3];
+
 
     Console.Write("salesperson: ");
-    var salesperson = Console.ReadLine().ToLower();
+    var salesperson = Convert.ToChar(Console.ReadLine().ToLower());
     var highestSale = 0;
-    var highest = "";
+    var highest = ' ';
     var grandTotal = 0;
-    while (salesperson != "z") {
-      switch (salesperson) {
-        case "d":
-        case "e":
-        case "f":
+
+    while (salesperson != 'z')
+    {
+      var isAllowed = false;
+      foreach (var allowedInitial in allowedInitials)
+      {
+
+        if (salesperson == allowedInitial)
+        {
+          isAllowed = true;
           Console.Write("sale: ");
           var sale = Convert.ToInt32(Console.ReadLine());
-          grandTotal+=sale;
-          if (sale > highestSale) {
+          grandTotal += sale;
+          if (sale > highestSale)
+          {
             highest = salesperson;
             highestSale = sale;
           }
-
-          break;
-          default:
-          Console.WriteLine("intermediate output: Error, invalid salesperson selected, please try again");
-            break;
+        }
       }
+      if (!isAllowed)
+      {
+        Console.WriteLine("intermediate output: Error, invalid salesperson selected, please try again");
+        
+      }
+
       Console.Write("salesperson: ");
-      salesperson = Console.ReadLine().ToLower();
+      salesperson = Convert.ToChar(Console.ReadLine().ToLower());
     }
+
+
     Console.WriteLine("Grand Total: {0:C0}", grandTotal);
-    Console.WriteLine("Highest Sale: {0}", highest.ToUpper());
+    Console.WriteLine("Highest Sale: {0}", Char.ToUpper(highest));
     Console.WriteLine("Passed");
   }
 }
